ebad60e7dc4fa011bd04b9d60f97f3a0d4f43306
karlinhos987@hotmail.com <karlinhos987@hotmail.com>
2022-04-27 21:28:32 -0300

    Adição de novas classes

--- Projeto/src/main/java/com/projetointegrador/projetointegrador/model/Cliente.java
+++ Projeto/src/main/java/com/projetointegrador/projetointegrador/model/Cliente.java
@@ -1,5 +1,23 @@
 package com.projetointegrador.projetointegrador.model;
 
-public class Cliente {
+import java.util.Date;
 
+public class Cliente implements Usuario {
+	private int idCliente;
+	private String email;
+	private String senha;
+	private String nome;
+	private String telefone;
+	private Date dataNascimento;
+	
+	@Override
+	public void cadastrar() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void alterarDados() {
+		// TODO Auto-generated method stub
+		
+	}
 }
