d55a801d723340140815e2f03bf9b38414ae98ef
Rejane Santos <rss.rejane@gmail.com>
2022-04-30 14:17:52 -0300

    Inclusão controller - endereço

---
+++ Projeto/src/main/java/com/projetointegrador/controller/EnderecoController.java
@@ -0,0 +1,63 @@
+package com.projetointegrador.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetointegrador.model.Endereco;
+import com.projetointegrador.repository.EnderecoRepository;
+
+@RestController
+@RequestMapping("/endereco")
+@CrossOrigin("*")
+public class EnderecoController {
+	
+	@Autowired
+	private EnderecoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Endereco>>GetAll()
+	{
+		return ResponseEntity.ok(repository.findAll());
+	}
+	@GetMapping("/{endereco}")
+	public ResponseEntity<Endereco> GetById(@PathVariable long id_endereco){
+		return repository.findById(id_endereco)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("endereco/{endereco}")
+	public ResponseEntity<List<Endereco>> GetByEndereco (@PathVariable String endereco){
+		return ResponseEntity.ok(repository.findAllByCepContainingIgnoreCase(endereco));
+	
+	}
+	
+	@PostMapping
+	public ResponseEntity<Endereco> post (@RequestBody Endereco endereco){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(endereco));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Endereco> put (@RequestBody Endereco endereco){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(endereco));
+	}
+	
+	@DeleteMapping("/{id_endereco}")
+	public void delete(@PathVariable long id_endereco) {
+		repository.deleteById(id_endereco);
+	}
+	
+
+}
