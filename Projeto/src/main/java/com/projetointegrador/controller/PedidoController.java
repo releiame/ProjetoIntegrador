66ca41a09e39443d62991abde43833ea06c4b74b
anamenici <anamenici@gmail.com>
2022-05-02 22:43:57 -0300

    Adicionando Controller de Pedido

---
+++ Projeto/src/main/java/com/projetointegrador/controller/PedidoController.java
@@ -0,0 +1,51 @@
+package com.projetointegrador.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.projetointegrador.model.Pedido;
+import com.projetointegrador.repository.PedidoRepository;
+
+public class PedidoController {
+
+	@Autowired
+	private PedidoRepository repositoryPedido;
+	
+	@GetMapping
+	public ResponseEntity<List<Pedido>>GetAll(){
+		return ResponseEntity.ok(repositoryPedido.findAll());
+	}
+	
+	@GetMapping("/{id_pedido}")
+	public ResponseEntity<Pedido> GetById(@PathVariable long id_pedido){
+		return repositoryPedido.findById(id_pedido)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Pedido> post (@RequestBody Pedido pedido){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoryPedido.save(pedido));
+	}
+	
+	@DeleteMapping("/{id_pedido}")
+	public void deletePedido(@PathVariable long id_pedido) {
+		repositoryPedido.deleteById(id_pedido);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Pedido> put (@RequestBody Pedido pedido){
+		return ResponseEntity.status(HttpStatus.OK).body(repositoryPedido.save(pedido));
+	}
+	
+	
+}
