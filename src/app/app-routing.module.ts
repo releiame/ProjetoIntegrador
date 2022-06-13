be9c3f062a8a1aac0afed0a54ffa0b5e5a5ca429
anamenici <anamenici@gmail.com>
2022-06-13 14:47:01 -0300

    Router: inserção de paths e routerLinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from "./components/modal/cadastro/cadastro.component";
+import { FaleConoscoComponent } from "./components/modal/fale-conosco/fale-conosco.component";
 import { LoginComponent } from "./components/modal/login/login.component";
 import { CarouselInicioComponent } from "./components/template/carousel-inicio/carousel-inicio.component";
 import { HeaderInicioDeslogComponent } from "./components/template/header-inicio-deslog/header-inicio-deslog.component";
 import { HomeComponent } from "./components/template/home/home.component";
+
+// --------------Substituir # por nome das respectivas classes-------------
 
 const routes: Routes = [
     {path: '', redirectTo:'home', pathMatch: 'full'},
@@ -12,7 +15,13 @@ const routes: Routes = [
     {path: 'login', component:LoginComponent},
     {path: 'cadastro', component:CadastroComponent},
     {path: 'header-inicio-log', component:HeaderInicioDeslogComponent},
+    {path: 'header-inicio-deslog', component:HeaderInicioDeslogComponent}
     {path: 'carousel', component:CarouselInicioComponent},
+    {path: 'fale-conosco', component:FaleConoscoComponent},
+    {path: 'minha-conta', component:#}, 
+    {path: 'meus-pedidos', component:#},
+    {path: 'trocar-devolver', component:#},
+    {path: 'carrinho', component:#}
 ];
 
 @NgModule({
