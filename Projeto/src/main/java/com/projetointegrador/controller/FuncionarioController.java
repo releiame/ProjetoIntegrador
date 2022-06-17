927d36d7a13ac8e13e1fe81d8af76578324b8223
Rejane Santos <rss.rejane@gmail.com>
2022-06-17 20:52:55 -0300

    Inclusão de endpoints na classe funcionario

--- Projeto/src/main/java/com/projetointegrador/controller/FuncionarioController.java
+++ Projeto/src/main/java/com/projetointegrador/controller/FuncionarioController.java
@@ -3,6 +3,8 @@ package com.projetointegrador.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projetointegrador.model.ClienteLogin;
 import com.projetointegrador.model.Funcionario;
+import com.projetointegrador.model.FuncionarioLogin;
 import com.projetointegrador.repository.FuncionarioRepository;
+import com.projetointegrador.service.FuncionarioService;
 
 @RestController //Indicando ao Spring que essa classe é um controller
 @RequestMapping("/funcionario") //O parametro o qual devo usar para acessar a classe
@@ -27,7 +32,10 @@ public class FuncionarioController {
 	@Autowired //Injeção de dependencia
 	private FuncionarioRepository repository;
 	
-	@GetMapping //Vai dizer que sempre que vier uma requisição externa
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@GetMapping ("/listartodos_funcionarios") //Vai dizer que sempre que vier uma requisição externa
 	public ResponseEntity<List<Funcionario>>GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 	} //Metodo para retornar todos o funcionarios cadastrados
@@ -59,4 +67,30 @@ public class FuncionarioController {
 	public void delete(@PathVariable long id_funcionario) {
 		repository.deleteById(id_funcionario);
 	} //Metodo de exclusão de um funcionario
-}
+	
+	@PostMapping("/logar_funcionario")
+	public ResponseEntity<FuncionarioLogin> Autentication(@RequestBody Optional<FuncionarioLogin> user)
+	{
+		return funcionarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar_funcionario")
+	public ResponseEntity<Funcionario> PostFuncionario (@Valid @RequestBody Funcionario funcionario)
+	{
+		return funcionarioService.CadastrarFuncionario(funcionario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+	
+	@PutMapping("/atualizar_funcionario")
+	public ResponseEntity<Funcionario> PutFuncionario (@Valid @RequestBody Funcionario funcionario)
+	{
+		return funcionarioService.CadastrarFuncionario(funcionario)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+			
+		}
+		
+	}
+	
