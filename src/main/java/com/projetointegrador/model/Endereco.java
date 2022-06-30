e19b94cdd89ef8a152074a733e00e38c355bf8a5
João Victor <jujotasurf@gmail.com>
2022-06-30 16:12:52 -0300

    Arrumando endereço

--- src/main/java/com/projetointegrador/model/Endereco.java
+++ src/main/java/com/projetointegrador/model/Endereco.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "tb_endereco")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Endereco {
 	
 	@Id
